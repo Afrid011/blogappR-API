74d16ed172d82b217e08a0cadd315b143651705f
Shaik Afrid <afrid0010@gmail.com>
2025-09-18 20:57:47 +0530

    enable CORS for localhost and render

--- src/main/java/com/sklassics/blogApp/BlogAppRApiApplication.java
+++ src/main/java/com/sklassics/blogApp/BlogAppRApiApplication.java
@@ -3,12 +3,30 @@ package com.sklassics.blogApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan("com.sklassics.blogApp.entity")
 public class BlogAppRApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BlogAppRApiApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BlogAppRApiApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry
+          .addMapping("/**")
+          .allowedOrigins(
+            "http://localhost:3000",
+            "https://blogappr-api-1.onrender.com"
+          );
+      }
+    };
+  }
 }
